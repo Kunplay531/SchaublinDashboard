b9fefea64038714f13fde77c96c8962591f8f6db
Gueniat Noël <noel.gueniat@students.bfh.ch>
2025-03-02 16:19:41 +0100

    first draft

---
+++ NPM-Folder/Simulation/PowerSimulator/PowerSimulator.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+import random
+
+async def power_sender_random(websocket):
+    print("Sending random values between 1 and 100 every second. Press Ctrl+C to stop.")
+    try:
+        while True:
+            random_value = str(random.randint(1, 100))
+            await websocket.send(random_value)
+            print(f"Sent random value: {random_value}")
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopped sending random values.")
+
+async def power_sender(websocket, path):
+    print("Client connected. Start entering RPM values.")
+    try:
+        while True:
+            # Take power input from the keyboard
+            power_value = input("Enter power percentage value to send: ")
+            # Send the power value to the WebSocket client
+            await websocket.send(power_value)
+            print(f"Sent RPM value: {power_value}")
+    except websockets.exceptions.ConnectionClosed:
+        print("Client disconnected.")
+
+async def main():
+    # Start the WebSocket server on specific ip and port 8001
+    async with websockets.serve(power_sender_random, "0.0.0.0", 8001):
+        print("WebSocket server is running on ws://0.0.0.0:8001")
+        await asyncio.Future()  # Run forever
+
+# Run the server
+asyncio.run(main())
---
+++ NPM-Folder/Simulation/RPMSimulator/RPMSimulator.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+import random
+
+async def rpm_sender_random(websocket):
+    print("Sending random values between 250 and 3000 every second. Press Ctrl+C to stop.")
+    try:
+        while True:
+            random_value = str(random.randint(250, 3000))
+            await websocket.send(random_value)
+            print(f"Sent random value: {random_value}")
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopped sending random values.")
+
+async def rpm_sender(websocket, path):
+    print("Client connected. Start entering RPM values.")
+    try:
+        while True:
+            # Take RPM input from the keyboard
+            rpm_value = input("Enter RPM value to send: ")
+            # Send the RPM value to the WebSocket client
+            await websocket.send(rpm_value)
+            print(f"Sent RPM value: {rpm_value}")
+    except websockets.exceptions.ConnectionClosed:
+        print("Client disconnected.")
+
+async def main():
+    # Start the WebSocket server on 0.0.0.0 and port 8080
+    async with websockets.serve(rpm_sender_random, "0.0.0.0", 8000):
+        print("WebSocket server is running on ws://0.0.0.0:8000")
+        await asyncio.Future()  # Run forever
+
+# Run the server
+asyncio.run(main())
